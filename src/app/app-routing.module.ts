6b0cb0b8b601b8fed3326f068c6396be9b83673e
Rohan Jadhav <jadhavrohan5050@gmail.com>
2025-12-29 10:13:45 +0530

    loaded component lazily

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { UsersComponent } from './pages/users/users.component';
-import { ProductsComponent } from './pages/products/products.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent) },
   {
     path: '',
     component: AppLayoutComponent,
     canActivate: [AuthGuard],
     children: [
-      { path: 'dashboard', component: DashboardComponent },
+      { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent) },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'users', component: UsersComponent },
-      { path: 'products', component: ProductsComponent }
+      { path: 'users', loadComponent: () => import('./pages/users/users.component').then((m) => m.UsersComponent) },
+      { path: 'products', loadComponent: () => import('./pages/products/products.component').then((m) => m.ProductsComponent) }
     ]
   }
 ];
